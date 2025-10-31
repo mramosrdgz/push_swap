9e8921e46b7f0e5e5077c640779f64d89e786ea0
mramosrdgz <mramos024.MR@gmail.com>
2025-10-31 12:35:10 +0100

    Refactor rev_rotate functions for code reuse
    Refactor reverse rotate functions to use a common helper.

--- src/ps_rev_rotate.c
+++ src/ps_rev_rotate.c
@@ -11,14 +11,14 @@
 /* ************************************************************************** */
 #include "push_swap.h"
 
-void	rev_rotate_a(t_list **stack_a)
+static void	rev_rotate_nodes(t_list **stack)
 {
 	t_list	*prev;
 	t_list	*last;
 
-	if (!stack_a || !(*stack_a) || !(*stack_a)-> next)
+	if (!stack || !(*stack) || !(*stack)-> next)
 		return ;
-	last = *stack_a;
+	last = *stack;
 	prev = NULL;
 	while (last -> next)
 	{
@@ -26,34 +26,25 @@ void	rev_rotate_a(t_list **stack_a)
 		last = last -> next;
 	}
 	prev -> next = NULL;
-	last -> next = *stack_a;
-	*stack_a = last;
+	last -> next = *stack;
+	*stack = last;
+}
+
+void	rev_rotate_a(t_list **stack_a)
+{
+	rev_rotate_nodes(stack_a);
 	write (1, "rra\n", 4);
 }
 
 void	rev_rotate_b(t_list **stack_b)
 {
-	t_list	*prev;
-	t_list	*last;
-
-	if (!stack_b || !(*stack_b) || !(*stack_b)-> next)
-		return ;
-	last = *stack_b;
-	prev = NULL;
-	while (last -> next)
-	{
-		prev = last;
-		last = last -> next;
-	}
-	prev -> next = NULL;
-	last -> next = *stack_b;
-	*stack_b = last;
+	rev_rotate_nodes(stack_b);
 	write (1, "rrb\n", 4);
 }
 
 void	rev_rotate_a_b(t_list **stack_a, t_list **stack_b)
 {
-	rev_rotate_a(stack_a);
-	rev_rotate_b(stack_b);
+	rev_rotate_nodes(stack_a);
+	rev_rotate_nodes(stack_b);
 	write (1, "rrr\n", 4);
 }
